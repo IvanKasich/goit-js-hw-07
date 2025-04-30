c91dc2fd9194d4028b54ea5e728a542874255656
Ivan Kasich <rebelkom@gmail.com>
2025-04-30 23:47:17 +0300

    fix task const

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,6 @@
-const elem = document.querySelector("input");
-
 const textInput = document.querySelector("input#name-input");
 const output = document.querySelector("span#name-output");
-elem.addEventListener("input", (event) => {
+textInput.addEventListener("input", (event) => {
   event.preventDefault();
   const inputValue = event.currentTarget.value.trim();
   if (inputValue !== "") {
