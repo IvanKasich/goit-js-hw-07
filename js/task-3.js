1379515e60b74c2003bd238a86e96b0a2de32718
Ivan Kasich <rebelkom@gmail.com>
2025-04-30 23:48:36 +0300

    preventDefault for forms

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,6 @@
 const textInput = document.querySelector("input#name-input");
 const output = document.querySelector("span#name-output");
 textInput.addEventListener("input", (event) => {
-  event.preventDefault();
   const inputValue = event.currentTarget.value.trim();
   if (inputValue !== "") {
     return (output.textContent = inputValue);
